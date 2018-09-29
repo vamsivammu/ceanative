248ae0e5728e0098566fb364a44e14baf4cbdd0e
krishnavamsi <vammuvamsi64@gmail.com>
2018-09-29 15:55:55 +0530

    tested axios with php and mysql

---
+++ screens/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$dbuser = "root";
+$dbpass= "";
+$dbname = "userinfo";
+$dbhost="localhost";
+$con= new mysqli($dbhost,$dbuser,$dbpass,$dbname);//two connections for accessing two tables
+$con1= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+$con2= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+$con3= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+$con4= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+$con5= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+?>
